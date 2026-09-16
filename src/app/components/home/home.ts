3eee6be3e45110bf55a42ebb01459cde24808cab
mdelaosa <m.delaosam@gmail.com>
2026-09-16 21:07:49 +0200

    home + styles

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user';
 
 @Component({
-  imports: [],
   selector: 'app-home',
-  styleUrl: './home.css',
+  standalone: true,
+  imports: [CommonModule], 
   templateUrl: './home.html',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home implements OnInit {
+  private userService = inject(UserService);
+  
+  users: any[] = [];
+
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe({
+      next: (data) => {
+        this.users = data.results || data; 
+        console.log('Usuarios cargados:', this.users);
+      },
+      error: (err) => {
+        console.error('Error al cargar los usuarios:', err);
+      }
+    });
+  }
+}
--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -1,4 +1,20 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
 
-@Service()
-export class User {}
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+  // La URL de donde vamos a sacar los datos
+    private apiUrl = 'https://peticiones.online/api/users';
+
+  // Inyectamos HttpClient para poder hacer peticiones web
+    constructor(private http: HttpClient) {}
+
+  // Método para pedir la lista de usuarios
+    getAllUsers(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+    }
+}
